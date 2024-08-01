dcb77cc615aaeb4f2183d9eeb8f90ed6727e8dac
0x0crypto <cozmoczm12@proton.me>
2024-08-01 11:17:37 -0300

    Add Response and Request struct

--- models/usersModel.go
+++ models/usersModel.go
@@ -1,8 +1,14 @@
 package models
 
-// User the user structure model
-type User struct {
+// Request user is structure for request
+type RequestUser struct {
 	ID   int64  `json:"id"`
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
+}
+
+// Response user is structure for response
+type ResponseUser struct {
 	Name string `json:"name"`
 	Age  int32  `json:"age"`
 }
